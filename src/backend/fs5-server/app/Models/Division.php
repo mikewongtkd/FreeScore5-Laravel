37d2092cbaeeb33c0df8667d0ed08d45e65d01ca
Mike Wong <mikewongtkd@gmail.com>
2021-12-31 17:10:05 -0800

    Divisions properly seeding

--- src/backend/fs5-server/app/Models/Division.php
+++ src/backend/fs5-server/app/Models/Division.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Division extends Model
 {
     use HasFactory;
+	protected $fillable = [ 'code', 'description', 'criteria', 'info' ];
+
 }
--- src/backend/fs5-server/database/seeders/DatabaseSeeder.php
+++ src/backend/fs5-server/database/seeders/DatabaseSeeder.php
@@ -24,30 +24,46 @@ class DatabaseSeeder extends Seeder
 		// \App\Models\Athlete::factory()->count( 200 )->create();
 
 		foreach( Config::$divisions as $division ) {
-			$athletes = DatabaseSeeder::eligible_athletes( $division[ 'criteria' ]);
+			$athletes = DatabaseSeeder::eligible_athletes( $division );
 			if( count( $athletes ) == 0 ) { continue; }
-			// \App\Models\Division::factory()->create( $division );
+
+			\App\Models\Division::factory()->create([
+				'code' => $division[ 'code' ],
+				'description' => $division[ 'description' ],
+				'criteria' => json_encode( $division[ 'criteria' ]),
+				'info' => json_encode([ 'difficulty' => $division[ 'difficulty' ], 'headcontactrules' => $division[ 'headcontactrules' ]])
+			]);
+
+			if( preg_match( '/grassroots/i', $division[ 'difficulty' ])) {
+			}
 		}
     }
 
 	/**
 	 * Reads the tournament configuration
 	 */
-	private static function eligible_athletes( $criteria ) {
-		$datemin = false;
-		$datemax = false;
+	private static function eligible_athletes( $division ) {
+		$criteria = $division[ 'criteria' ];
+		$query = \DB::table( 'athletes' );
+
+		// Prepare special criteria
 		Config::age_range_to_dates( $criteria[ 'age' ], $criteria, 'dob' );
 		Config::rank_range_to_list( $criteria[ 'rank' ], $criteria, 'rank' );
 
-		$query = \DB::table( 'athletes' );
+		// Apply all criteria
 		Config::apply_criteria( $query, $criteria, 'gender' );
 		Config::apply_criteria( $query, $criteria, 'dob' );
 		Config::apply_criteria( $query, $criteria, 'rank' );
 
-		$sql      = str_replace( array( '?' ), array( '\'%s\'' ), $query->toSql());
-		$sql      = vsprintf( $sql, $query->getBindings());
+		$debug_sql = false;
+		if( $debug_sql ) {
+			$sql = str_replace( array( '?' ), array( '\'%s\'' ), $query->toSql());
+			$sql = vsprintf( $sql, $query->getBindings());
+			dump( $sql );
+		}
+
+		// Run query
 		$athletes = $query->get();
-		if( count( $athletes ) > 0 ) { dump( $athletes, $sql ); }
 		return $athletes;
 	}
 
